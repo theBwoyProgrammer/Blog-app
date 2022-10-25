a0cc6fe37deb543b3af08f7bbc87fd7532ad7d99
Codeaddictjaver <102610715+Javernick@users.noreply.github.com>
2022-10-25 23:35:44 +0300

    deleted junk

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,13 +7,13 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 
-rails generate migration CreateUsers name:string photo:text bio:text posts_counter:integer
-rails g migration CreatePosts title:string text:text comment_counter:integer likes_counter:integer 
-rails g migration CreateComments text:text
-rails g migration CreateLikes
-rails g migration AddUserRefToPosts user:references:index
-rails g migration AddUserRefToComments user:references:index
-rails g migration AddUserRefToLikes user:references:index
-rails g migration AddPostRefToComments post:references:index
-rails g migration AddPostRefToLikes post:references:index
-rails db:migrate
+# rails generate migration CreateUsers name:string photo:text bio:text posts_counter:integer
+# rails g migration CreatePosts title:string text:text comment_counter:integer likes_counter:integer 
+# rails g migration CreateComments text:text
+# rails g migration CreateLikes
+# rails g migration AddUserRefToPosts user:references:index
+# rails g migration AddUserRefToComments user:references:index
+# rails g migration AddUserRefToLikes user:references:index
+# rails g migration AddPostRefToComments post:references:index
+# rails g migration AddPostRefToLikes post:references:index
+# rails db:migrate
