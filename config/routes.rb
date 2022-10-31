fb2729c123765c433e3ca652ec767c2fb8c16cca
Codeaddictjaver <102610715+Javernick@users.noreply.github.com>
2022-10-31 18:04:04 +0300

    controllers and routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get '/posts' => 'posts#index'
+  get '/posts/show' => 'posts#show'
+  get '/users' => 'users#index'
+  get '/users/show' => 'users#show'
 end
