2611e3d78dd57ab2e8c05a82b7abce2dd80ed80f
Codeaddictjaver <102610715+Javernick@users.noreply.github.com>
2022-11-05 19:33:09 +0300

    rails forms

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,23 @@
+class CommentsController < ApplicationController
+  def new
+    @id = current_user.id
+    @comment = Comment.new
+  end
+
+  def create
+    post = Post.find(params[:id])
+    comment = Comment.new(author: current_user, post:, text: comments_params[:text])
+    if comment.save
+      flash[:notice] = 'Comment created successfully'
+    else
+      flash[:error] = 'Error'
+    end
+    redirect_to user_post_comments_path
+  end
+
+  private
+
+  def comments_params
+    params.require(:comment).permit(:text)
+  end
+end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,8 @@ class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
 
+  after_save :update_comments_counter
+
   def update_comments_counter
     post.update(comments_counter: post.comments.count)
   end
