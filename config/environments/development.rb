540ae2c94695c6ebfb20948976b454b80316acb9
Codeaddictjaver <102610715+Javernick@users.noreply.github.com>
2022-11-09 21:51:44 +0300

    devise final

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,6 +11,25 @@ Rails.application.configure do
     Bullet.add_footer    = true
   end
 
+  # Devise authentication
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_caching = false
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: "testdevelop404@gmail.com",
+    password: "ozuejgktojcrcbet", 
+    domain: "smtp.gmail.com",
+    openssl_verify_mode: "none",
+  }
+
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
